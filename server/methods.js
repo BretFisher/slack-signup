ac26561ec197566af2a84b1883e2f6cc5af124a2
Bret Fisher <bret@fishbrains.com>
2015-01-15 10:12:56 -0500

    changed admin channel name

--- server/methods.js
+++ server/methods.js
@@ -22,7 +22,7 @@ Meteor.methods({
     var icon = ":slack:";
 
     // Slack channel to post to
-    var channel = "organizers";
+    var channel = "coreteam";
 
     // make actual webhook call
     try {
